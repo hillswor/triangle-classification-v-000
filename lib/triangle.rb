d5dc435cdfb4a89a9ac2d21d5770b8cf43399b90
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-24 02:16:22 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,16 @@
 class Triangle
-  def initialize(side_length_one, side_length_two, side_length_three)
-    @side_length_one = side_length_one
-    @side_length_two = side_length_two
-    @side_length_three = side_length_three
+  #sl1 = Side Length 1, sl2 = Side Length 2, sl3 = Side Length 3
+  def initialize(sl1, sl2, sl3)
+    @sl1 = sl1
+    @sl2 = sl2
+    @sl3 = sl3 
+    @side_lengths = []
   end
 
   def kind
+    if @side_lengths.any? {|sl| sl == 0}
+      
+
+  def kind
+    if 
 end
